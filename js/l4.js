b8d08074cd8630bc6e4193de3b605557a6d35259
Akhil sharma <168325399+Akhil1sharma@users.noreply.github.com>
2025-12-09 08:39:08 +0000

    comparison of datatype

---
+++ js/l4.js
@@ -0,0 +1,40 @@
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue); // -3
+
+console.log(2-2);
+console.log(2 + (-2)); // 0
+console.log(2*2);
+console.log(4/2);
+console.log(5%2); // 1
+console.log(2**3); // 8 
+
+let str1 = "Hello ";
+let str2 = "World";
+let fullStr = str1 + str2;
+console.log(fullStr); // "Hello World"
+
+console.log("1" + 2); // "12"
+console.log(1 + "2");
+console.log("1" + "2"); // "12"
+
+let gamecounter = 100;
+gamecounter ++; // gamecounter = gamecounter + 1
+console.log(gamecounter); // 101
+
+let points = 50;
+++points;
+console.log(points); // 51
+
+console.log("2" > 1); // true   
+console.log("02" > 1); // true
+
+// THESE TYPE OF COMPARISONS CAN CAUSE BUGS IN YOUR CODE and create unecessary confusion
+// console.log (null >= 0); // true
+// console.log (null > 0); // false
+// console.log (null == 0); // false
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
